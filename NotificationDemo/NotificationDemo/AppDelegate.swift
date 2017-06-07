35deed9cdecc1712000df6ec25c896dd334fabb5
zz <zz@zzdeMac-mini.local>
2017-06-07 17:32:00 +0800

    Notification

--- NotificationDemo/NotificationDemo/AppDelegate.swift
+++ NotificationDemo/NotificationDemo/AppDelegate.swift
@@ -9,15 +9,16 @@
 import UIKit
 import UserNotifications
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate ,UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        self.getCurrentPushSettings()
-        self.getNotHandledNotifications()
+//        self.getCurrentPushSettings()
+//        self.getNotHandledNotifications()
+//        UNUserNotificationCenter.current().delegate = self
         self.authorizeAPNS()
         self.configureNotificationCategory()
         self.schedualLocalNotification()
@@ -29,7 +30,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         center.getNotificationSettings { (notificationSettings) in
             print("\(notificationSettings)")
         }
-
     }
     
     func registerApns() {
@@ -60,10 +60,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                               title: "Stop",
                                               options: .foreground)
         
+        
+        //This action is delivered only if the notification’s category object was configured with the customDismissAction option. To trigger this action, the user must explicitly dismiss the notification interface. For example, the user must tap the Dismiss button or swipe down on the notification interface in watchOS to trigger this action.
         let expiredCategory = UNNotificationCategory(identifier: "TIMER_EXPIRED",
                                                      actions: [snoozeAction, stopAction],
                                                      intentIdentifiers: [],
-                                                     options: UNNotificationCategoryOptions(rawValue: 0))
+                                                     options: .customDismissAction)
         
         // Register the notification categories.
         let center = UNUserNotificationCenter.current()
@@ -75,16 +77,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         content.title = NSString.localizedUserNotificationString(forKey: "Wake up!", arguments: nil)
         content.body  = NSString.localizedUserNotificationString(forKey: "Rise and shine!It's morning time!", arguments: nil)
         content.sound = UNNotificationSound.default()
-        content.categoryIdentifier = "TIMER_EXPIRED"
+        content.categoryIdentifier = "GENERAL"
         
         //configure the trigger for a 7am wakeup
         var dateInfo = DateComponents()
-        dateInfo.hour = 17
-        dateInfo.minute = 0
+        dateInfo.hour = 14
+        dateInfo.minute = 44
         
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateInfo, repeats: false)
         
-        let request = UNNotificationRequest(identifier: "MorningAlarm", content: content, trigger: trigger)
+        // Swift
+        let timeIntervalTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 120,
+                                                        repeats: false)
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateInfo, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "MorningAlarm", content: content, trigger: timeIntervalTrigger)
         
         let center = UNUserNotificationCenter.current()
         center.add(request) { (error:Error?) in
@@ -92,7 +98,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 print(err.localizedDescription)
             }
         }
-        
     }
     
     func getNotHandledNotifications() {
@@ -128,13 +133,114 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let device_ns = NSData.init(data: deviceToken)
         
-        let token:String = device_ns.description.trimmingCharacters(in: CharacterSet(charactersIn: "<>" )).replacingOccurrences(of: " ", with: "")
+        let pushMeToken = device_ns.description.trimmingCharacters(in: CharacterSet(charactersIn: "<>" ))
+        print(pushMeToken)
         
-        print(token)
+        
+        let phpToken:String = pushMeToken.replacingOccurrences(of: " ", with: "")
+        print(phpToken)
     }
     
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("\(error)")
+    }
+    
+    
+    /*UIAppDelegate for notification*/
+    //When a remote notification arrives, the system handles user interactions normally when the app is in the background
+    //You can use these methods to examine the payload and perform any related tasks. For example, upon receiving a silent remote notification, you might start downloading new content for your app. 
+    //Failure to execute the completion handler block at the end of your implementation will cause your app to be terminated.
+//    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+//        print(userInfo)
+//    }
+    
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        print(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceive notification: UILocalNotification) {
+        print(notification.alertBody!)
+    }
+    
+    //Failure to execute the completion handler block at the end of your implementation will cause your app to be terminated.
+    func application(_ application: UIApplication, handleActionWithIdentifier identifier: String?, for notification: UILocalNotification, completionHandler: @escaping () -> Void) {
+        print(identifier!)
+        completionHandler()
+    }
+    
+    //For apps that want to initiate background downloads, the system calls this function when the time is right for you to start those downloads.
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+    }
+    
+    
+    /*UNUserNotificationCenterDelegate*/
+    //called when the app is in foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // Update the app interface directly.
+        print("A silent notification received!")
+        // Play a sound.
+        completionHandler(UNNotificationPresentationOptions.sound);
+    }
+    
+    //Failure to execute the completion handler block at the end of your implementation will cause your app to be terminated.
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.actionIdentifier == UNNotificationDismissActionIdentifier {
+            // The user dismissed the notification without taking action
+            print("The user dismissed the notification without taking action")
+        }else if response.actionIdentifier == UNNotificationDefaultActionIdentifier {
+            // The user launched the app
+            print("The user launched the app")
+        }else{
+            if response.notification.request.content.categoryIdentifier == "TIMER_EXPIRED" {
+                // Handle the actions for the expired timer.
+                if response.actionIdentifier == "SNOOZE_ACTION" {
+                    // Invalidate the old timer and create a new one. . .
+                    print("The user tap the snooze button!")
+                } else if response.actionIdentifier == "STOP_ACTION" {
+                    // Invalidate the timer. . .
+                    print("The user tap the stop button!")
+                }
+            }
+        }
+        completionHandler()
+    }
+    
+    
+    /*Why can‘t UNUserNotificationCenterDelegate be this class*/
+    class CustomNotificationCenterDelegate: NSObject,UNUserNotificationCenterDelegate {
+        //called when the app is in foreground
+        func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            // Update the app interface directly.
+            print("A silent notification received!")
+            // Play a sound.
+            completionHandler(UNNotificationPresentationOptions.sound);
+        }
+        
+        func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+            if response.actionIdentifier == UNNotificationDismissActionIdentifier {
+                // The user dismissed the notification without taking action
+                print("The user dismissed the notification without taking action")
+            }
+            else if response.actionIdentifier == UNNotificationDefaultActionIdentifier {
+                // The user launched the app
+                print("The user launched the app")
+            }else{
+                if response.notification.request.content.categoryIdentifier == "TIMER_EXPIRED" {
+                    // Handle the actions for the expired timer.
+                    if response.actionIdentifier == "SNOOZE_ACTION" {
+                        // Invalidate the old timer and create a new one. . .
+                        print("The user tap the snooze button!")
+                    }
+                    else if response.actionIdentifier == "STOP_ACTION" {
+                        // Invalidate the timer. . .
+                        print("The user tap the stop button!")
+                    }
+                }
+            }
+        }
+
     }
 }
 
