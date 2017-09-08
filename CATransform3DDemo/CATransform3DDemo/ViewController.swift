10ec94e35020c8f975a8a6a6e435702995117751
WelcomeToCodingWorld <747852289@qq.com>
2017-09-09 00:49:24 +0800

    failed to load create pointer

--- CATransform3DDemo/CATransform3DDemo/ViewController.swift
+++ CATransform3DDemo/CATransform3DDemo/ViewController.swift
@@ -10,28 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let imageUrl = "/Users/arili/Desktop/screenshot-a.png"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let sourceTypes = CGImageSourceCopyTypeIdentifiers()
-        CFShow(sourceTypes)
-        
-        let destinationTypes = CGImageDestinationCopyTypeIdentifiers()
-        CFShow(destinationTypes)
+//        let sourceTypes = CGImageSourceCopyTypeIdentifiers()
+//        CFShow(sourceTypes)
+//
+//        let destinationTypes = CGImageDestinationCopyTypeIdentifiers()
+//        CFShow(destinationTypes)
+        guard image != nil else {
+            print("image creat failed")
+            return;
+        }
         
     }
     
-    lazy var image = {
-        let url = URL(fileURLWithPath: "")
+    var image : CGImage? {
+        let url = URL(fileURLWithPath: imageUrl)
         let myImage : CGImage?
         let myImageSource : CGImageSource?
         var myOptions : CFDictionary?
         
-        var key1:UnsafeMutablePointer<CFString> = UnsafeMutablePointer.allocate(capacity: 1)
+        let key1:UnsafeMutablePointer<CFString> = UnsafeMutablePointer.allocate(capacity: 1)
         
         key1.initialize(to: kCGImageSourceShouldCache, count: 1)
         
-        var key2:UnsafeMutablePointer<CFString> = UnsafeMutablePointer.allocate(capacity: 1)
+        let key2:UnsafeMutablePointer<CFString> = UnsafeMutablePointer.allocate(capacity: 1)
         
         key2.initialize(to: kCGImageSourceShouldAllowFloat, count: 1)
         
@@ -43,27 +49,59 @@ class ViewController: UIViewController {
         let value2:UnsafeMutablePointer<CFBoolean> = UnsafeMutablePointer.allocate(capacity: 1)
         
         value2.initialize(to: kCFBooleanTrue, count: 1)
-        var myKeys = [key1,key2]
+        let myKeys = [key1,key2]
         let myValues = [value1,value2]
         
-        var keyPointer:UnsafeMutablePointer<UnsafeRawPointer?> = UnsafeMutablePointer.allocate(capacity: 1)
+        let keyPointer:UnsafeMutablePointer<UnsafeRawPointer?> = UnsafeMutablePointer.allocate(capacity: 1)
         keyPointer.initialize(to: myKeys, count: 1)
         
-        var valuePointer : UnsafeMutablePointer<UnsafeRawPointer?> = UnsafeMutablePointer.allocate(capacity: 1)
+        let valuePointer : UnsafeMutablePointer<UnsafeRawPointer?> = UnsafeMutablePointer.allocate(capacity: 1)
         
         keyPointer.initialize(to: myValues, count: 1)
         
         
 
         
-        var callBackKeyPointer : UnsafeMutablePointer<CFDictionaryKeyCallBacks> = UnsafeMutablePointer.allocate(capacity: 1)
+        let callBackKeyPointer : UnsafeMutablePointer<CFDictionaryKeyCallBacks> = UnsafeMutablePointer.allocate(capacity: 1)
         callBackKeyPointer.initialize(to: kCFTypeDictionaryKeyCallBacks, count: 1)
         
-        var callBackValuePointer : UnsafeMutablePointer<CFDictionaryValueCallBacks> = UnsafeMutablePointer.allocate(capacity: 1)
+        let callBackValuePointer : UnsafeMutablePointer<CFDictionaryValueCallBacks> = UnsafeMutablePointer.allocate(capacity: 1)
         callBackValuePointer.initialize(to: kCFTypeDictionaryValueCallBacks, count: 1)
         
         
         myOptions = CFDictionaryCreate(nil, keyPointer, valuePointer, 2, callBackKeyPointer, callBackValuePointer)
+        
+        myImageSource = CGImageSourceCreateWithURL(url as CFURL, myOptions)
+        guard myImageSource != nil else {
+            print("image source is null")
+            return nil;
+        }
+        
+        myImage = CGImageSourceCreateImageAtIndex(myImageSource!, 0, nil)
+        
+        guard myImage != nil else {
+            print("image not create from image source")
+            return nil;
+        }
+        
+        key1.deinitialize()
+        key1.deallocate(capacity: 1)
+        key2.deinitialize()
+        key2.deallocate(capacity: 1)
+        value1.deinitialize()
+        value1.deallocate(capacity: 1)
+        value2.deinitialize()
+        value2.deallocate(capacity: 1)
+        keyPointer.deinitialize()
+        keyPointer.deallocate(capacity: 1)
+        valuePointer.deinitialize()
+        valuePointer.deallocate(capacity: 1)
+        callBackKeyPointer.deinitialize()
+        callBackKeyPointer.deallocate(capacity: 1)
+        callBackValuePointer.deinitialize()
+        callBackValuePointer.deallocate(capacity: 1)
+        
+        return myImage!
 
     }
     
