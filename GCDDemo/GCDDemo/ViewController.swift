b030f15ddfe9112dee1ec4218210ccd9670e8ce3
zz <zz@zzdeMac-mini.local>
2017-10-20 17:26:11 +0800

    GCD

--- GCDDemo/GCDDemo/ViewController.swift
+++ GCDDemo/GCDDemo/ViewController.swift
@@ -10,10 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var testLab: UILabel!
+    var time:UInt = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        
+        // MARK:- GCD
+        let group = DispatchGroup()
+        let workItem1 = DispatchWorkItem.init(block: {[weak self] in
+            DispatchQueue.main.async {
+                self?.testLab.bounds = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 100, height: 30))
+                self?.testLab.text = "Brown"
+                print("Brown")
+            }
+        })
+        
+        let workItem2 = DispatchWorkItem.init(block: {[weak self] in
+            DispatchQueue.main.async {
+                self?.testLab.textAlignment = .center
+                self?.testLab.center = (self?.view.center)!
+                self?.testLab.text = "James"
+                print("James")
+            }
+        })
+        
+        
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async(group: group, execute: workItem1)
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async(group: group, execute: workItem2)
+        
+        let workItem3 = DispatchWorkItem.init(block: {[weak self] in
+            DispatchQueue.main.async {
+                self?.testLab.textAlignment = .center
+                self?.testLab.center = (self?.view.center)!
+                self?.testLab.text = "Kyrie Irving"
+                print("Kyrie Irving")
+            }
+        })
+        
+        workItem3.notify(queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.default)) {
+            print("What's going on!")
+        }
+        
+        workItem3.notify(queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.default), execute: workItem2)
+        
+
+        DispatchQueue.global().async {
+            print("Let me go first")
+        }
+        
+        //Sets the target queue for the given object.
+        
+        // MARK:- Operation
+        let operation1 = BlockOperation.init {
+            print("this is a blockOperation")
+        }
+        
+        operation1.completionBlock = {
+            print("the operation is completed")
+        }
+        
+        let operation2 = BlockOperation.init {
+            print("this is the second blockOperation")
+        }
+        
+        operation1.addDependency(operation2)
+        OperationQueue.current?.addOperation(operation1)
+        OperationQueue.current?.addOperation(operation2)
     }
 
     override func didReceiveMemoryWarning() {
