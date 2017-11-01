993ce42d338d2d4ee17f606dfcf3bb77f8ea37f8
zz <zz@zzdeMac-mini.local>
2017-11-01 16:44:21 +0800

    UILayoutGuide

--- Test-OC-Swift/Sources/ViewController.swift
+++ Test-OC-Swift/Sources/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
         view.addSubview(sureBtn)
         view.addSubview(cancelBtn)
         
+        //test snapKit
         sureBtn.snp.makeConstraints { (maker) in
             maker.left.equalToSuperview().offset(15)
             maker.width.equalTo(75)
@@ -36,7 +37,76 @@ class ViewController: UIViewController {
             maker.centerY.equalTo(sureBtn)
         }
         
+        let container = UIView()
+        container.layer.borderColor = UIColor.black.cgColor
+        container.layer.borderWidth = 1
+        view.addSubview(container)
+        container.snp.makeConstraints { (maker) in
+            maker.top.equalTo(sureBtn.snp.bottom).offset(20)
+            maker.left.equalToSuperview().offset(15)
+            maker.right.equalToSuperview().offset(-15)
+            maker.height.equalTo(300)
+        }
         
+        
+        //test layoutGuide
+        let label1 = UILabel.label(text: "label1", textColor: UIColor.blue, textFont: UIFont.systemFont(ofSize: 15))
+        label1.layer.borderWidth = 1
+        label1.layer.borderColor = UIColor.brown.cgColor
+        let label2 = UILabel.label(text: "label2", textColor: UIColor.blue, textFont: UIFont.systemFont(ofSize: 15))
+        label2.layer.borderWidth = 1
+        label2.layer.borderColor = UIColor.brown.cgColor
+        let label3 = UILabel.label(text: "label3", textColor: UIColor.blue, textFont: UIFont.systemFont(ofSize: 15))
+        label3.layer.borderWidth = 1
+        label3.layer.borderColor = UIColor.brown.cgColor
+        container.addSubview(label1)
+        container.addSubview(label2)
+        container.addSubview(label3)
+        
+        let space1 = UILayoutGuide()
+        let space2 = UILayoutGuide()
+        container.addLayoutGuide(space1)
+        container.addLayoutGuide(space2)
+        
+        local {
+            space1.snp.makeConstraints { (maker) in
+                maker.height.equalToSuperview()
+                maker.width.equalTo(20)
+                maker.left.equalTo(container.snp.centerX).offset(-50)
+                maker.centerY.equalTo(container.snp.centerY)
+            }
+            
+            space2.snp.makeConstraints { (maker) in
+                maker.height.equalTo(space1)
+                maker.width.equalTo(space1)
+                maker.left.equalTo(container.snp.centerX).offset(50)
+                maker.centerY.equalTo(container.snp.centerY)
+            }
+            
+            label1.snp.makeConstraints { (maker) in
+                maker.height.equalTo(30)
+                maker.width.equalTo(75)
+                maker.top.equalToSuperview().offset(15)
+                maker.right.equalTo(space1.snp.left)
+            }
+            
+            label2.snp.makeConstraints { (maker) in
+                maker.height.equalTo(30)
+                maker.width.equalTo(75)
+                maker.top.equalToSuperview().offset(15)
+                maker.left.equalTo(space1.snp.right)
+                maker.right.equalTo(space2.snp.left)
+            }
+            
+            label3.snp.makeConstraints { (maker) in
+                maker.height.equalTo(30)
+                maker.width.equalTo(75)
+                maker.top.equalToSuperview().offset(15)
+                maker.left.equalTo(space2.snp.right)
+            }
+            
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
